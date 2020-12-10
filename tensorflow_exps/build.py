32d8ff83d8aa416c06537a97772b05e33d3b7175
kcreativision <>
2020-12-10 23:06:48 +0530

    repo update

--- tensorflow_exps/build.py
+++ tensorflow_exps/build.py
@@ -1,6 +1,10 @@
 import tensorflow as tf
 import numpy as np
 import argparse
+import datetime
+from pathlib import Path
+import shutil
+import os
 
 
 class AccuracyCallback(tf.keras.callbacks.Callback):
@@ -11,7 +15,7 @@ class AccuracyCallback(tf.keras.callbacks.Callback):
             self.model.stop_training = True
 
 
-class BuildTF(object):
+class TF(object):
     def __init__(self, file_info, params):
         self.file_info = file_info
         self.params = params
@@ -21,42 +25,81 @@ class BuildTF(object):
 
         self.x_train, self.y_train = np.array, np.array
         self.x_test, self.y_test = np.array, np.array
+        # self.data_stats()
+        self.model = None
+        self.base_stats = ''
+
+    def init(self):
         self.read_data()
         self.preprocess_data()
-        # self.data_stats()
 
     def assert_fileinfo(self):
-        assert 'training_data' in self.file_info.keys(), 'training_data in file_info is compulsory'
+        assert 'save_loc' in self.file_info.keys(), 'save_loc in file_info is compulsory'
 
     def assert_params(self):
-        if 'normalise_method' not in self.params.keys():
-            self.params['normalise_method'] = None
+        # TODO check if multiple callbacks can be used
         if 'callback' not in self.params.keys():
             self.params['callback'] = None
-        assert 'tf_compile' in self.params.keys(), "compile params are compulsory"
+
+        params_default = {'training_data': 'fashion_mnist', 'normalise_method': 'pixel',
+                          'tf_compile': {'optimizer': 'sgd', 'loss': 'sparse_categorical_crossentropy',
+                                         'metrics': ['accuracy'], 'epochs': 10},
+                          'mode': 'basic_dense'}
+
+        if 'normalise_method' not in self.params.keys():
+            self.params['normalise_method'] = params_default['normalise_method']
+        if 'training_data' not in self.params.keys():
+            self.params['training_data'] = params_default['training_data']
+        if 'mode' not in self.params.keys():
+            self.params['mode'] = params_default['mode']
+        if 'tf_compile' not in self.params.keys():
+            self.params['tf_compile'] = params_default['tf_compile']
+        if 'optimizer' not in self.params['tf_compile'].keys():
+            self.params['tf_compile']['optimizer'] = params_default['tf_compile']['optimizer']
+        if 'loss' not in self.params['tf_compile'].keys():
+            self.params['tf_compile']['loss'] = params_default['tf_compile']['loss']
+        if 'metrics' not in self.params['tf_compile'].keys():
+            self.params['tf_compile']['metrics'] = params_default['tf_compile']['metrics']
+        if 'epochs' not in self.params['tf_compile'].keys():
+            self.params['tf_compile']['epochs'] = params_default['epochs']['metrics']
+
+        if 'create_unique' in self.params.keys():
+            assert self.params['create_unique'] in [True, False]
+        else:
+            self.params['create_unique'] = False
+
+        if self.params['create_unique']:
+            self.params['log_dir_suffix'] = '_' + datetime.datetime.now().strftime("%Y%m%d-%H%M%S")
+        else:
+            self.params['log_dir_suffix'] = ''
 
     def read_data(self):
-        if self.file_info['training_data'] == 'fashion_mnist':
-            print('reading fashion_mnist', end=' ', flush=True)
+        if self.params['training_data'] == 'fashion_mnist':
+            self.base_stats += 'reading fashion_mnist \n'
             fmnist = tf.keras.datasets.fashion_mnist
             (self.x_train, self.y_train), (self.x_test, self.y_test) = fmnist.load_data()
-            print('x_train shape: {0}'.format(np.shape(self.x_train)), end=' ', flush=True)
-            print('x_test shape: {0}'.format(np.shape(self.x_train)))
+            self.base_stats += 'shapes: \n\t x_train: {0} \n\t y_train: {1} \n\t x_test: {2} \n\t y_test: {3} \n'.format(
+                np.shape(self.x_train), np.shape(self.x_train), np.shape(self.x_test), np.shape(self.y_test))
+        elif self.params['training_data'] == 'mnist':
+            self.base_stats += 'reading mnist \n'
+            mnist = tf.keras.datasets.mnist
+            (self.x_train, self.y_train), (self.x_test, self.y_test) = mnist.load_data()
+            self.base_stats += 'shapes: \n\t x_train: {0} \n\t y_train: {1} \n\t x_test: {2} \n\t y_test: {3} \n'.format(
+                np.shape(self.x_train), np.shape(self.x_train), np.shape(self.x_test), np.shape(self.y_test))
         else:
-            raise NotImplementedError
+            raise AssertionError('available training dataset options: mnist/fashion_mnist')
 
     def preprocess_data(self):
         if self.params['normalise_method'] == 'pixel':
-            print('normalising by 255.0')
+            self.base_stats += 'normalising by 255.0\n'
             self.x_train, self.x_test = self.x_train / 255.0, self.x_test / 255.0
         elif self.params['normalise_method'] is None:
-            print('not normalising the input data, neural nets might not converge')
+            self.base_stats += 'not normalising the input data, neural nets might not converge\n'
             pass
         else:
             raise NotImplementedError
 
     def build(self):
-
         self.model = tf.keras.models.Sequential([
             tf.keras.layers.Conv2D(64, (3, 3), activation='relu', input_shape=(28, 28, 1)),
             tf.keras.layers.MaxPooling2D(2, 2),
@@ -66,7 +109,7 @@ class BuildTF(object):
             tf.keras.layers.Dense(1024, activation=tf.nn.relu),
             tf.keras.layers.Dense(10, activation=tf.nn.softmax)
         ])
-        # TODO check binayr_cross_entropy for binary classification
+        # TODO check binary_cross_entropy for binary classification
         # TODO check RMSProp as optimizer as we can tweak learning rate
         self.model.compile(optimizer=self.params['tf_compile']['optimizer'],
                       loss=self.params['tf_compile']['loss'],
@@ -77,19 +120,67 @@ class BuildTF(object):
         elif self.params['callback'] is None:
             self.model.fit(self.x_train, self.y_train, epochs=10)
 
+    def basic_dense(self):
+        self.model = tf.keras.models.Sequential([tf.keras.layers.Flatten(),
+                                                 tf.keras.layers.Dense(128, activation=tf.nn.relu),
+                                                 tf.keras.layers.Dense(20, activation=tf.nn.softmax)])
+
+        self.model.compile(optimizer=self.params['tf_compile']['optimizer'],
+                           loss=self.params['tf_compile']['loss'],
+                           metrics=self.params['tf_compile']['metrics'])
+
+        log_dir = self.file_info['save_loc'] + \
+                  "logs/fit/{0}_basic_dense_{1}epochs".format(self.params['training_data'],
+                                                              self.params['tf_compile']['epochs']) + \
+                  self.params['log_dir_suffix']
+        if Path(log_dir).exists() and Path(log_dir).is_dir():
+            print('log directory already exists, deleting {0}'.format(log_dir))
+            shutil.rmtree(log_dir)
+        print('creating log directory {0}'.format(log_dir))
+        os.makedirs(log_dir)
+
+        tensorboard_callback = tf.keras.callbacks.TensorBoard(log_dir=log_dir, histogram_freq=1)
+        fit_history = self.model.fit(self.x_train, self.y_train, epochs=self.params['tf_compile']['epochs'],
+                       callbacks=[tensorboard_callback], verbose=True)
+        eval_history = self.model.evaluate(self.x_test, self.y_test, verbose=True)
+
+        filename = log_dir + '/train_report.txt'
+        with open(filename, 'w+') as fh:
+            fh.write('\n\n\n' + '#'*20 + '\ndata statistics: \n' + '#'*20 + '\n')
+            fh.write(self.base_stats)
+
+            fh.write('\n\n\n' + '#' * 20 + '\n' + 'model fit history: \n' + '#' * 20 + '\n')
+            fh.write('params: \n')
+            for k in fit_history.params.keys():
+                fh.write(str(k) + ':' + str(fit_history.params[k]) + '\n')
+
+            fh.write('\nloss, accuracy metrics: \n')
+            for i in fit_history.epoch:
+                fh.write('Epoch {0}/{1} - loss: {2:=0.4f} - accuracy: {3:=0.4f} \n'.format(i, len(fit_history.epoch),
+                         fit_history.history['loss'][i], fit_history.history['accuracy'][i]))
+
+            fh.write('\n\n\n' + '#'*20 + '\n' + 'model summary: \n' + '#'*20 + '\n')
+            self.model.summary(print_fn=lambda x: fh.write(x + '\n'), line_length=80)
+
+            fh.write('\n\n\n' + '#' * 20 + '\n' + 'model evaluate history: \n' + '#' * 20 + '\n')
+            fh.write('loss: {0:=0.4f} - accuracy: {1:=0.4f}'.format(eval_history[0], eval_history[1]))
+
+    def build_model(self):
+        if self.params['mode'] == 'basic_dense':
+            self.basic_dense()
+        else:
+            raise NotImplementedError
+
 
 if __name__ == '__main__':
-    parser = argparse.ArgumentParser(description='TF build')
-    parser.add_argument('--file_info', help='file info')
-    parser.add_argument('--params', help='params')
+    parser = argparse.ArgumentParser(description='TF Build and Visualize')
+    parser.add_argument('--file_info', help='file info', nargs='?', const='{}', type=str)
+    parser.add_argument('--params', help='params', nargs='?', const='{}', type=str)
     args = parser.parse_args()
-    # TODO add default args
-    parent_file_info = eval(args.file_info)
-    parent_params = eval(args.params)
-    print('file_info', parent_file_info)
-    print('params')
-    for k in parent_params.keys():
-        print(k, type(parent_params[k]),parent_params[k])
+    file_info = eval(args.file_info)
+    params = eval(args.params)
 
-    tf_model = BuildTF(parent_file_info, parent_params)
-    tf_model.build()
+    tf_model = TF(file_info=file_info, params=params)
+    tf_model.init()
+    tf_model.build_model()
+
--- tensorflow_exps/visualize.py
+++ tensorflow_exps/visualize.py
@@ -1,10 +1,10 @@
 import tensorflow as tf
 import numpy as np
 import argparse
-from tensorflow_exps.build import BuildTF
+from tensorflow_exps.build import TF
 
 
-class VisualizeTF(BuildTF):
+class VisualizeTF(TF):
     """
     Visualizations on
         1. Convolution layers
@@ -36,9 +36,11 @@ class VisualizeTF(BuildTF):
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='TF Build and Visualize')
-    # TODO add default args
-    parser.add_argument('--file_info', help='file info')
-    parser.add_argument('--params', help='params')
+    parser.add_argument('--file_info', help='file info', nargs='?', const='{}', type=str)
+    params_default = "{'training_data':'fashion_mnist','normalise_method': 'pixel', " \
+                     "'tf_compile': {'optimizer': 'sgd', 'loss': 'sparse_categorical_crossentropy', " \
+                     "'metrics': ['accuracy']}}"
+    parser.add_argument('--params', help='params', nargs='?', const=params_default, type=str)
     args = parser.parse_args()
 
     parent_file_info = eval(args.file_info)
@@ -48,7 +50,7 @@ if __name__ == '__main__':
     for k in parent_params.keys():
         print(k, type(parent_params[k]),parent_params[k])
 
-    tf_model = BuildTF(file_info=parent_file_info, params=parent_params)
+    tf_model = TF(file_info=parent_file_info, params=parent_params)
     tf_model.build()
 
     tf_graphs = VisualizeTF(model=tf_model.model, file_info=parent_file_info, params=parent_params)
